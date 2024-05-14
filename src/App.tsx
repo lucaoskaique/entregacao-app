cc88dc79dadcc1d2201b6fa6f08f437fea0a707a
VGarcia <vfeitosa.garcia@gmail.com>
2024-05-14 20:25:47 -0300

    Add: documentação do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
-import "./styles/app.css"
+import { About } from "./components/About";
+import { Cta } from "./components/Cta";
+import { FAQ } from "./components/FAQ";
+import { Features } from "./components/Features";
+import { Footer } from "./components/Footer";
+import { Hero } from "./components/Hero";
+import { HowItWorks } from "./components/HowItWorks";
+import { Navbar } from "./components/Navbar";
+import { Newsletter } from "./components/Newsletter";
+import { Pricing } from "./components/Pricing";
+import { ScrollToTop } from "./components/ScrollToTop";
+import { Services } from "./components/Services";
+import { Sponsors } from "./components/Sponsors";
+import { Team } from "./components/Team";
+import { Testimonials } from "./components/Testimonials";
+import "./styles/App.css";
 
 function App() {
   return (
     <>
-      
+      <Navbar />
+      <Hero />
+      <Sponsors />
+      <About />
+      <HowItWorks />
+      <Features />
+      <Services />
+      <Cta />
+      <Testimonials />
+      <Team />
+      <Pricing />
+      <Newsletter />
+      <FAQ />
+      <Footer />
+      <ScrollToTop />
     </>
   );
 }
