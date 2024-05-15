af9da3c282f3fccb39a53b3a6c117cb7ac8d7eff
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2024-05-15 18:24:48 -0300

    refactor: uncomment imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-// import { About } from './components/About'
+import { About } from './components/About'
 // import { Cta } from './components/Cta'
-// import { FAQ } from './components/FAQ'
+import { FAQ } from './components/FAQ'
 // import { Features } from './components/Features'
 import { Footer } from './components/Footer'
 import { Hero } from './components/Hero'
