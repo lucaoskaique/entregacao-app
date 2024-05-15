19847422c8a7b955c7b28cd214b31653eb457ba6
VGarcia <vfeitosa.garcia@gmail.com>
2024-05-15 16:20:22 -0300

    feat: add theme switcher

--- src/components/switchTheme.tsx
+++ src/components/switchTheme.tsx
@@ -1,14 +1,22 @@
 import { Switch } from "./ui/switch"
+import { useTheme } from "@/components/theme-provider";
 import { Moon, Sun } from "lucide-react"
 
 
-const SwitchTheme = () => {
-  return(
-    <>
+const SwitchTheme = (props: {className?:string}) => {
+  const { theme, setTheme } = useTheme();
+  
+  return (
+    <div className={props.className}>
       <Sun className="h-[1.1rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Switch />
-      <Moon className="absolute h-[1.1rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-    </>
+      <Switch
+        defaultChecked={(window.matchMedia("(prefers-color-scheme: dark)").matches)}
+        onCheckedChange={(checked) => {
+          if (checked) setTheme("dark")
+          if (!checked) setTheme("light")
+        }} />
+      <Moon className=" h-[1.1rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    </div>
   )
 }
 
