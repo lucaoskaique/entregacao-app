32547c6f1449952dac8eff48e60de1eddf95206f
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2024-05-15 18:26:32 -0300

    refactor: move container instagram sos to a new div

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,10 +6,6 @@ export const Footer = () => {
   return (
     <section className='bg-neutral-900 px-[15px] pb-[56.41px] pt-7'>
       <div>
-        <div className='bg-[#262626] border border-[#212121] flex items-center justify-around h-[150px] rounded-xl'>
-          <img src={InstagramIcon} alt='Ícone do Instagram' />
-          <span className='text-white font-bold text-2xl'>@sos.cozinhas</span>
-        </div>
         <div className='flex flex-col items-center my-[41px]'>
           <img src={Logo} alt='Logo' className='w-[55.59px] h-[53px]' />
           <span className='text-white text-3xl'>entregação</span>
@@ -31,6 +27,10 @@ export const Footer = () => {
         <div className='bg-white flex flex-col rounded-lg items-center py-[11px] gap-[10px] my-[24px]'>
           <span>Apoio:</span>
           <img src={SOS} alt='Cozinha SOS RS' className='w-[85px] h-[69px]' />
+        </div>
+        <div className='bg-[#262626] border border-[#212121] flex items-center justify-around h-[150px] rounded-xl'>
+          <img src={InstagramIcon} alt='Ícone do Instagram' />
+          <span className='text-white font-bold text-2xl'>@sos.cozinhas</span>
         </div>
         <div className='text-neutral-300 border-t-2 border-neutral-800'>
           <div className='flex flex-col items-center mt-[22px]'>
