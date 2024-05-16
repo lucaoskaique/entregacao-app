14fd4551344eee92b136e70a643e9b7a40838f33
VGarcia <vfeitosa.garcia@gmail.com>
2024-05-16 13:45:33 -0300

    refactor: changed old image button

--- src/components/ui/button-wpp-hero.tsx
+++ src/components/ui/button-wpp-hero.tsx
@@ -1,11 +1,11 @@
-import Whatsapp from '@/assets/images/whatsapp-icon.svg'
+import WhatsappIcon from '@/assets/images/icons/whatsapp-icon.svg'
 
 export default function ButtonWpp() {
     return (
         <>
-            <button type='button' className='bg-[#00A324] text-white w-full flex items-center justify-between p-5 rounded-sm text-base font-semibold mb-6'>
+            <button type='button' className='bg-wppButton text-white w-full flex items-center justify-between p-5 rounded-sm text-base font-semibold mb-6'>
                 Quero Ajudar
-                <img src={Whatsapp} alt='Ícone do whatsapp' />
+                <img src={WhatsappIcon} alt='Ícone do whatsapp' />
             </button>
         </>
     )
