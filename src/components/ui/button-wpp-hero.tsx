62e9916091d0355643b6101a39a626ee5e34afdb
Filipe Dos Santos <filipesantos.psic@gmail.com>
2024-05-16 13:53:24 -0300

    bugfix: add whatsapp link

--- src/components/ui/button-wpp-hero.tsx
+++ src/components/ui/button-wpp-hero.tsx
@@ -1,12 +1,16 @@
 import Whatsapp from '@/assets/images/whatsapp-icon.svg'
 
-export default function ButtonWpp() {
+interface ButtonWppProps {
+    whatsappLink: string;
+}
+
+export default function ButtonWpp({ whatsappLink }: ButtonWppProps) {
     return (
         <>
-            <button type='button' className='bg-[#00A324] text-white w-full flex items-center justify-between p-5 rounded-sm text-base font-semibold mb-6'>
+            <a role='button'  href={whatsappLink} target='_blank' rel="noopener noreferrer" className='bg-[#00A324] text-white w-full flex items-center justify-between p-5 rounded-sm text-base font-semibold mb-6'>
                 Quero Ajudar
                 <img src={Whatsapp} alt='Ícone do whatsapp' />
-            </button>
+            </a>
         </>
     )
 }
