63a22103ece78fa68eb3b7c533ff8193e0231bea
Antonio Rossett <tonyrossett@hotmail.com>
2024-05-17 16:08:33 -0300

    feat: add whatsapp link

--- src/components/ui/button-wpp-hero.tsx
+++ src/components/ui/button-wpp-hero.tsx
@@ -8,15 +8,11 @@ interface ButtonWppType {
 export default function ButtonWpp({ title, whatsappLink }: ButtonWppType) {
     return (
         <>
-<<<<<<< Updated upstream
+
             <a role='button' href={whatsappLink} target='_blank' rel="noopener noreferrer" className='bg-wppButton text-white w-full flex items-center justify-between p-5 rounded-sm text-xl font-inter-bold mb-6'>
                 {title}
                 <WhatsappIcon width={24} height={24} />
-=======
-            <a role='button'  href={whatsappLink} target='_blank' rel="noopener noreferrer" className='bg-wppButton text-white w-full flex items-center justify-between p-5 rounded-sm text-base font-semibold mb-6'>
-                Quero Ajudar
-                <WhatsappIcon />
->>>>>>> Stashed changes
+
             </a>
         </>
     )
