b5a2203b155ca872bcde226e93788f81ad83fbc8
VGarcia <vfeitosa.garcia@gmail.com>
2024-05-20 16:36:59 -0300

    refactor: remove logo absolute position

--- src/components/Logos.tsx
+++ src/components/Logos.tsx
@@ -48,7 +48,7 @@ export const ResponsiveLogoIcon = () => {
   }
 }
 
-export const ResponsiveLogoName = ({className = "absolute inset-x-1/2 -translate-x-2/4"}:{className?:string}) => {
+export const ResponsiveLogoName = ({className}:{className?:string}) => {
   const { theme } = useTheme()
   if(theme==="dark"){
     return <LogoName className={className} color="white" height={45} />
