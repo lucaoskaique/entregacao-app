89cd2083567f3b5ef1ab89882ef03160e72009ea
lucaoskaique <lucas.kaique175@hotmail.com>
2024-05-30 20:25:36 -0300

    feat: testes feriado

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import { About } from './components/ui/about/about'
-import { FAQ } from './components/ui/faq/faq'
-import { Footer } from './components/ui/footer/footer'
-import { Hero } from './components/ui/hero/hero-section'
-import { Navbar } from './components/ui/header/navbar'
-import { ScrollToTop } from './components/ui/scroll-to-top'
-import './styles/app.css'
+import { About } from "./components/ui/about/about";
+import { FAQ } from "./components/ui/faq/faq";
+import { Footer } from "./components/ui/footer/footer";
+import { Hero } from "./components/ui/hero/hero-section";
+import { Navbar } from "./components/ui/header/navbar";
+import { ScrollToTop } from "./components/ui/scroll-to-top";
+import "./styles/app.css";
 
 function App() {
   return (
     <>
+      <Navbar />
+      <Navbar />
+      <Navbar />
+      <Navbar />
+      <Navbar />
       <Navbar />
       <Hero />
       <About />
