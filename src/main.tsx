39bcb3f35c4bca34109a50942218268f1aa16f78
VGarcia <vfeitosa.garcia@gmail.com>
2024-06-17 12:36:33 -0300

    refactor: realocate ThemeProvider from main to App

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { ThemeProvider } from "@/components/ui/theme-provider.tsx";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider>
       <App />
-    </ThemeProvider>
   </React.StrictMode>
 );
